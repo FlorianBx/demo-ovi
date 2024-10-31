4575629cf863848226b295fd52a4b1980b7a6141
Florian <25250981+BFlorian91@users.noreply.github.com>
2024-10-31 17:08:52 +0100

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+// import { AuthGuard } from './core/guards/auth-guard';
+// import { AdminGuard } from './core/guards/admin-guard.guard';
+// import { StudentsGuard } from './core/guards/students-guard.guard';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component'
+// import { Admin } from './pages/admin/admin.component';
+// import { Students } from './pages/students/students.component';
+//
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
+//   { path: 'students', loadChildren: () => import('./pages/students/students.module').then(m => m.StudentsModule) },
+//   { path: '**', redirectTo: '/home', pathMatch: 'full' },
+];
